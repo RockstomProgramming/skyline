26c9c1d6ca61fa1d742cbbc887a614f121ecf8e1
Wesley Luiz <lespaulg1@gmail.com>
2015-12-16 11:23:05 -0200

    Optimização da munição e plano de fundo

--- src/Game.js
+++ src/Game.js
@@ -1,30 +1,42 @@
 // Definição de variaveis locais
 MyGame.Game = function (game) {
-
-    // Camadas
-    this.fundoLayer, this.player, this.aviaoLayer = null;
-    this.velocidade = 5;
+    this.velocidade = 3;
     this.velocidadeMissel = 5;
     this.tempoMissel = 0;
+    this.tempoBala = 0;
     this.tiro = [];
+    this.qntArvores = 15;
+    this.elementosMapa = [];
+    this.qntMissil = 10;
 };
 
 MyGame.Game.prototype = {
 
     create: function () {
         // Camadas
-        this.aviaoLayer = this.add.group();
-        this.aviaoLayer.z = 1;
-
         this.fundoLayer = this.add.group();
         this.fundoLayer.z = 0;
 
+        this.mapaLayer = this.add.group();
+        this.mapaLayer.z = 1
+
+        this.aviaoLayer = this.add.group();
+        this.aviaoLayer.z = 2;
+        
         // Distribuição dos elementos nas camadas
+        this.fundo = this.add.sprite(0, 0, 'background');
+        this.fundoLayer.add(this.fundo);
+
+        this.rand = new Phaser.RandomDataGenerator();
+
+        for (var i = 0; i < this.qntArvores; i++) {
+            var elemento = this.add.sprite(this.rand.between(0,800), this.rand.between(0,600), 'arvore');
+            this.mapaLayer.add(elemento);
+            this.elementosMapa.push(elemento);
+        }
+
         this.player = this.add.sprite(400, 400, 'jogador');
         this.aviaoLayer.add(this.player);
-
-        //this.fundo = this.add.sprite(0, 0, 'background');
-        //this.fundoLayer.add(this.fundo);
 
         //this.explosao = this.add.sprite(30, 30, 'explosao');
         //this.explosao.animations.add('explodir', [0, 1, 2, 3, 4], 15, true);
@@ -41,7 +53,7 @@ MyGame.Game.prototype = {
         this.physics.startSystem(Phaser.Physics.ARCADE);
 
         this.cursor = this.input.keyboard.createCursorKeys();
-        this.btnAcao = this.input.keyboard.addKey([Phaser.Keyboard.SPACEBAR]);
+        this.btnAcao = this.input.keyboard.addKey([Phaser.Keyboard.SPACEBAR, Phaser.Keyboard.SHIFT, Phaser.Keyboard.R]);
     },
 
     update: function() {
@@ -59,32 +71,71 @@ MyGame.Game.prototype = {
              this.player.play('centro');
         }
 
-        if (this.btnAcao.isDown) {
-            this.atirar();
-        }
+        this.atirar();
 
         if (this.tiro.length > 0) {
             for (var i = 0; i < this.tiro.length; i++) {
                 var tiro = this.tiro[i];
                 tiro.y -= this.velocidadeMissel;
+
+                if (tiro.y < -600) {
+                    this.tiro.splice(this.tiro.indexOf(tiro), 1);
+                }
             }
+        }
+
+        this.recarregarMissil();
+        this.atualizarElementos();
+    },
+
+    recarregarMissil: function() {
+        if (this.input.keyboard.isDown(Phaser.Keyboard.R)) {
+            this.qntMissil = 10;
         }
     },
 
     atirar: function() {
-        if (this.time.now > this.tempoMissel) {
-            var x = this.player.x + 15;
-            var y = this.player.y - 10;
+        if (this.input.keyboard.isDown(Phaser.Keyboard.SHIFT)) {
+            if (this.time.now > this.tempoMissel && this.qntMissil > 0) {
+                var x = this.player.x + 15;
+                var y = this.player.y - 10;
 
-            missil = this.add.sprite(x, y, 'missil');
+                missil = this.add.sprite(x, y, 'missil');
 
-            this.tiro.push(missil);
+                this.tiro.push(missil);
 
-            this.tempoMissel = this.time.now + 200;
+                this.tempoMissel = this.time.now + 200;
+                this.qntMissil --;
+            }
+        } else if (this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+            if (this.time.now > this.tempoBala) {
+                var x = this.player.x + 15;
+                var y = this.player.y - 10;
+
+                bala = this.add.sprite(x, y, 'bala');
+
+                this.tiro.push(bala);
+
+                this.tempoBala = this.time.now + 200;
+            }
+        }
+    },
+
+    atualizarElementos: function () {
+        if (this.elementosMapa.length > 0) {
+            for (var i = 0; i < this.elementosMapa.length; i++) {
+                if (this.elementosMapa[i].y >= this.game.height) {
+                    this.elementosMapa[i].y = 0;
+                    this.elementosMapa[i].x = this.rand.between(0,800);
+                }
+
+                this.elementosMapa[i].y += this.velocidade + 2;
+            }
         }
     },
 
     render: function () {
-        this.game.debug.text('Mísseis Lançados: ' + this.tiro.length, 32, 32);
+        this.game.debug.text('Disparos Ativos: ' + this.tiro.length, 32, 32);
+        this.game.debug.text('Mísseis: ' + this.qntMissil, 32, 64);
     }
 };
--- src/Preloader.js
+++ src/Preloader.js
@@ -9,8 +9,10 @@ MyGame.Preloader.prototype = {
 		// - Carrega todos os assets -
         this.load.image('fundoTitulo', 'images/backGroundTitle.png');
         this.load.image('iniciar', 'images/play.png');
-        this.load.image('background', 'images/space.png');
-        this.load.image('missil', 'images/missil.png', 10, 13);
+        this.load.image('background', 'images/mapa.png');
+        this.load.image('arvore', 'images/arvore.png');
+        this.load.image('missil', 'images/missil.png');
+        this.load.image('bala', 'images/bala2.png');
         this.load.spritesheet('jogador', 'images/player.png', 40, 64);
         this.load.spritesheet('explosao', 'images/explode.png', 26, 26);
 	},
